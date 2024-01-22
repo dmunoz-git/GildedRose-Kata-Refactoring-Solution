097cafe6e4b38f1898bf6ef44ec709e19ad54f21
dmunoz-git <dmunozGit@gmail.com>
2024-01-22 14:54:46 +0100

    Add new feacture test

--- gilded-rose-kata/src/test/java/com/gildedrose/GildedRoseTest.java
+++ gilded-rose-kata/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,9 +8,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 
 @TestInstance(Lifecycle.PER_CLASS)
 class GildedRoseTest {	
+	private final int MAX_ITEM_QUATITY = 50;
+	private final int MAX_SULFURAS_QUANTITY = 80;
+	
 	private Item[] items;
 	private GildedRose app;
 	
@@ -26,9 +30,11 @@ class GildedRoseTest {
                 new Item("Sulfuras, Hand of Ragnaros", 0, 80),
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 40),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 40),
+                new Item("Backstage passes to a TAFKAL80ETC concert", -1, 40),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", -1, 49),
+                new Item("Conjured", 5, 40)
 		};
 		
 		this.app = new GildedRose(this.items);
@@ -71,19 +77,19 @@ class GildedRoseTest {
 	@Test
 	public void testMaxItemQuality() {
 		app.updateQuality();
-		assertEquals(50, items[5].quality, "Max quality value must be 50");
+		assertEquals(MAX_ITEM_QUATITY, items[5].quality, "Max quality value must be 50");
 	}
 
 	@Test
 	public void testSulfurasMaxQuality() {
 		app.updateQuality();
-		assertEquals(80, items[6].quality, "Sulfuras quality mustn't be higher than 80");
+		assertEquals(MAX_SULFURAS_QUANTITY, items[6].quality, "Sulfuras quality mustn't be higher than 80");
 	}
 
 	@Test
 	public void testSulfurasStaticQuality() {
 		app.updateQuality();
-		assertEquals(80, items[6].quality, "Sulfuras quality mustn't decrement");
+		assertEquals(MAX_SULFURAS_QUANTITY, items[6].quality, "Sulfuras quality mustn't decrement");
 	}
 
 	@Test
@@ -110,13 +116,28 @@ class GildedRoseTest {
 	public void testBackstateIncrementQualityByThree() {
 		int quality = items[10].quality;
 		app.updateQuality();
-		assertEquals(quality + 3, items[9].quality, "Backstage ticket quality must increment by two when there are 5 days or less");
+		assertEquals(quality + 3, items[10].quality, "Backstage ticket quality must increment by two when there are 5 days or less");
 	}
 
 	@Test
 	public void testBackstateOutdatedQuality() {
 		app.updateQuality();
-		assertEquals(0, items[10].quality, "Backstage ticket drop to 0 after the concert");
+		assertEquals(0, items[11].quality, "Backstage ticket drop to 0 after the concert");
+	}
+	
+	@Test
+	public void testMaxBackstageIncrementQuality() {
+		app.updateQuality();
+		assertEquals(MAX_ITEM_QUATITY, items[12].quality, "Backstage max ticket quality must be 50 even if we increment by two or three");
+	}
+	
+	@Disabled
+	@Test 
+	void testConjuredDecrementQuality() {
+		int quality = items[13].quality;
+		app.updateQuality();
+		assertEquals(quality - 2, items[13].quality, "Backstage max ticket quality must be 50 even if we increment by two or three");
+
 	}
 
 }
